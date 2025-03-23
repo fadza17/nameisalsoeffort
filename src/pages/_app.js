9bd7528358eb5cc007ce3630cbed379d3fc028b8
fadza17 <110185935+fadza17@users.noreply.github.com>
2025-03-23 09:19:58 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 // src/pages/_app.js
 import { GameProvider } from '../context/GameContext';
-import '.../nameisalsoeffort/src/styles/global.css';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
