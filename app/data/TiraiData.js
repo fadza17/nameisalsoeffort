f271c84fea2ddc55f08811effac8889304ceeb96
fadza17 <110185935+fadza17@users.noreply.github.com>
2025-03-23 11:00:34 +0700

    Create TiraiData.js

---
+++ app/data/TiraiData.js
@@ -0,0 +1,42 @@
+export const tiraiData = [
+  {
+    id: 1,
+    image: "/images/tirai1.jpeg",
+    revealImage: "/images/destinasi1.jpg",
+    destinasi: "Jalan-jalan di Mall",
+    deskripsi: "Kita akan menjelajahi mall dari ujung ke ujung (Hartono, Amplaz, atau JCM). Tantangannya: jalan tanpa arah, masuk ke store, lihat-lihat tanpa beli (beli = opsional). Yang ngeluh cape dapat punishment traktir apapun yang diinginkan pemenang!",
+    sound: "/sounds/tirai1.mp3"
+  },
+  {
+    id: 2,
+    image: "/images/tirai2.jpg",
+    revealImage: "/images/destinasi2.jpeg",
+    destinasi: "Jalan-jalan di Malioboro",
+    deskripsi: "Kita akan jalan dari ujung ke ujung Malioboro tapi ga boleh duduk, atau duduk hanya 1 menit. Yang kalah ada punishment spesial!",
+    sound: "/sounds/tirai2.mp3"
+  },
+  {
+    id: 3,
+    image: "/images/tirai3.jpg",
+    revealImage: "/images/destinasi3.jpeg",
+    destinasi: "Nonton Film Bareng",
+    deskripsi: "Kita main batu gunting kertas, yang menang nentuin film. Challenge-nya: selama film berjalan sampai selesai gaboleh ngerasa bosen. Yang bosen duluan ada hukumannya!",
+    sound: "/sounds/tirai3.mp3"
+  },
+  {
+    id: 4,
+    image: "/images/tirai4.jpg",
+    revealImage: "/images/destinasi4.jpg",
+    destinasi: "Nongkrong di Angkringan",
+    deskripsi: "Kita akan nongkrong di angkringan depan rel kereta api Malioboro. Syaratnya: harus terus ngobrol tanpa henti! Kalau salah satu dari kita 1 menit ga mulai obrolan, orang tersebut kalah dan harus bayarin makan siang!",
+    sound: "/sounds/tirai4.mp3"
+  },
+  {
+    id: 5,
+    image: "/images/tirai5.jpeg",
+    revealImage: "/images/destinasi5.jpeg",
+    destinasi: "Healing ke Kaliurang",
+    deskripsi: "Kita akan pergi ke Kaliurang untuk menikmati udara sejuk dan pemandangan gunung. Tantangannya: ambil foto yang paling aesthetic untuk kenang-kenangan! Yang fotonya paling bagus jadi juara dan dapat hadiah special!",
+    sound: "/sounds/tirai5.mp3"
+  }
+];
