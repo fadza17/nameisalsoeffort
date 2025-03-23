be1028fe8a4db9c9bfa7be2386beebb77f2dddd7
fadza17 <110185935+fadza17@users.noreply.github.com>
2025-03-23 10:05:40 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,18 @@
+'use client';
+
+import { useGameContext } from '@/context/GameContext';
+import HomePage from '@/components/HomePage';
+import QuizSection from '@/components/QuizSection';
+import TiraiSection from '@/components/TiraiSection';
+
+export default function Home() {
+  const { gameStage } = useGameContext();
+  
+  return (
+    <div className="main-container">
+      {gameStage === 'home' && <HomePage />}
+      {gameStage === 'quiz' && <QuizSection />}
+      {gameStage === 'tirai' && <TiraiSection />}
+    </div>
+  );
+}
